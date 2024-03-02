7c75107327967c7f9d3ae74774be99328ae23759
DiligentCoder-20022001 <siddharthschandran45@gmail.com>
2024-03-02 23:04:25 +0530

    Notion Integration going on

---
+++ client/src/components/Topic-Box/TopicBox.js
@@ -0,0 +1,17 @@
+import './TopicBox.css';
+
+function TopicBox() 
+{
+    return (
+        <div className = "topicClass">
+            <div className = "titleBox">
+                <div className = "title">
+                    <p>Arrays</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+
+export default TopicBox;
