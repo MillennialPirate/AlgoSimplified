d15661b46986077d1b51443bac4a354ed7848c55
DiligentCoder-20022001 <siddharthschandran45@gmail.com>
2024-03-05 00:53:52 +0530

    Notion Integration Completed

---
+++ backend/Services/NotionService.js
@@ -0,0 +1,71 @@
+const { json } = require("express");
+const fetch = require('node-fetch');
+
+firstNotionCall = async(notionKey, pageId) => {
+    var jsonString = '';
+    try{
+        const response = await fetch(`https://api.notion.com/v1/blocks/${pageId}/children/`, {
+            method: 'GET', 
+            headers: {
+                'Authorization': `Bearer ${notionKey}`, 
+                'Notion-Version': '2021-08-16'
+            }
+        }); 
+
+        if(!response.ok){
+            throw new Error('Failed to fetch page content'); 
+        }
+        const data = await response.json(); 
+        jsonString = JSON.stringify(data);
+    }
+    catch(error){
+        console.error('Error', error); 
+        throw error;
+    }
+    return jsonString;
+}
+
+nonFirstNotionCalls = async(notionKey, pageId, startCursor) => {
+    var jsonString = '';
+    try{
+        const response = await fetch(`https://api.notion.com/v1/blocks/${pageId}/children?start_cursor=${startCursor}`, {
+            method: 'GET', 
+            headers: {
+                'Authorization': `Bearer ${notionKey}`, 
+                'Notion-Version': '2021-08-16'
+            }
+        }); 
+
+        if(!response.ok){
+            throw new Error('Failed to fetch page content'); 
+        }
+        const data = await response.json(); 
+        jsonString = JSON.stringify(data);
+    }
+    catch(error){
+        console.error('Error', error); 
+        throw error;
+    }
+    return jsonString;
+}
+
+getNotionPageContent = async() => {
+    const notionKey = 'secret_aWn3CJOYdUNJvQ7DKaOz5RDAQNJpiOakDHN4viMeW1L'; 
+    const pageId = 'f190f07d24dc46248dbe20edd077ab32'; 
+
+    var next_cursor = '';
+    var jsonCombinedString = [];
+    
+    var firstJsonString = await firstNotionCall(notionKey, pageId); 
+    jsonCombinedString.push(firstJsonString);
+    next_cursor = JSON.parse(firstJsonString).next_cursor;
+    
+    while(next_cursor != null){
+        var jsonString = await nonFirstNotionCalls(notionKey, pageId, next_cursor); 
+        jsonCombinedString.push(jsonString); 
+        next_cursor = JSON.parse(jsonString).next_cursor; 
+    }
+    console.log(jsonCombinedString);
+}   
+
+module.exports = {getNotionPageContent};
